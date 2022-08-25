8bddba844b7db45dc69f79046f7f6f45fd2c5f94
Ricky Sophin <sophinricky@gmail.com>
2022-08-25 13:42:38 -0400

    changed db client connection

--- db/client.js
+++ db/client.js
@@ -11,13 +11,7 @@ let client;
 
 // github actions client config
 if (process.env.CI) {
-  client = new Client({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
-  });
+  client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/cyborg');
 } else {
   // local / heroku client config
   client = new Client(DB_URL);
